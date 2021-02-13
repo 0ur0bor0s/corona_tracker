983bd0dcc5e957b63544772798306270bd9c81d6
0ur0bor0s <ben.foley3214@gmail.com>
2021-02-12 21:40:12 -0800

    cleanup

--- data.py
+++ data.py
@@ -23,7 +23,8 @@ def parse_and_graph_data():
     confirm_path = os.path.join(script_dir, 'csv/time_series_covid19_confirmed_global.csv')
     death_path = os.path.join(script_dir, 'csv/time_series_covid19_deaths_global.csv')
     recov_path = os.path.join(script_dir, 'csv/time_series_covid19_recovered_global.csv')    
-    
+    csv_paths = [confirm_path, death_path, recov_path]
+
 
     # [confirmed, deaths, recoveries]
     china_data = [[],[],[]]
@@ -37,114 +38,53 @@ def parse_and_graph_data():
     """
     Parse Data
     """
-    # parse confirm cases
-    with open(confirm_path) as confirm_file:
-        confirm_reader = csv.reader(confirm_file, delimiter=',')
+
+    i = 0
+    for path in csv_paths:
+        with open(path) as file:
+            reader = csv.reader(file, delimiter=',')
+
+            # get list of dates in first iteration
+            if i == 0:
+                header = next(reader)
+                for date in itertools.islice(header, 4, None):
+                    dates.append(date)
+
+            # confirm_case_arr = [row for idx, row in enumerate(reader) if idx in ()]
+            for row in reader:
+                if row[1] == 'China':
+                    # initially populate cell
+                    if len(china_data[i]) == 0:
+                        for cell in itertools.islice(row, 4, None):
+                            china_data[i].append(int(cell))
+                    else:
+                        # add new row to current cases
+                        for j in range(4, len(row)-4):
+                            china_data[i][j] += int(row[j])
+                elif row[1] == 'Italy':
+                    for cell in itertools.islice(row, 4, None):
+                        italy_data[i].append(int(cell))
+                elif row[1] == 'Germany':
+                    for cell in itertools.islice(row, 4, None):
+                        germany_data[i].append(int(cell))
+                elif row[1] == 'Iran':
+                    for cell in itertools.islice(row, 4, None):
+                        iran_data[i].append(int(cell))
+                elif row[1] == 'US':
+                    # initially populate cell
+                    if len(us_data[i]) == 0:
+                        for cell in itertools.islice(row, 4, None):
+                            us_data[i].append(int(cell))
+                    else:
+                        # add new row to current cases
+                        for j in range(4, len(row)-4):
+                            us_data[i][j] += int(row[j])
         
-        # get list of dates
-        header = next(confirm_reader)
-        for date in itertools.islice(header, 4, None):
-            dates.append(date)
+        # update index
+        i += 1
 
-        #confirm_case_arr = [row for idx, row in enumerate(reader) if idx in ()]
-        for row in confirm_reader:
-            if row[1] == 'China':
-                # initially populate cell
-                if len(china_data[0]) == 0:
-                    for cell in itertools.islice(row, 4, None):
-                        china_data[0].append(int(cell))
-                else:
-                    # add new row to current cases
-                    for index in range(4, len(row)-4):
-                        china_data[0][index] += int(row[index])
-            elif row[1] == 'Italy':
-                for cell in itertools.islice(row, 4, None):
-                    italy_data[0].append(int(cell))
-            elif row[1] == 'Germany':
-                for cell in itertools.islice(row, 4, None):
-                    germany_data[0].append(int(cell))
-            elif row[1] == 'Iran':
-                for cell in itertools.islice(row, 4, None):
-                    iran_data[0].append(int(cell))
-            elif row[1] == 'US':
-                # initially populate cell
-                if len(us_data[0]) == 0:
-                    for cell in itertools.islice(row, 4, None):
-                        us_data[0].append(int(cell))
-                else:
-                    # add new row to current cases
-                    for index in range(4, len(row)-4):
-                        us_data[0][index] += int(row[index])
-
-    # parse death case
-    with open(death_path) as death_file:
-        death_reader = csv.reader(death_file, delimiter=',')
-
-        #confirm_case_arr = [row for idx, row in enumerate(reader) if idx in ()]
-        for row in death_reader:
-            if row[1] == 'China':
-                # initially populate cell
-                if len(china_data[1]) == 0:
-                    for cell in itertools.islice(row, 4, None):
-                        china_data[1].append(int(cell))
-                else:
-                    # add new row to current cases
-                    for index in range(4, len(row)-4):
-                        china_data[1][index] += int(row[index])
-            elif row[1] == 'Italy':
-                for cell in itertools.islice(row, 4, None):
-                    italy_data[1].append(int(cell))
-            elif row[1] == 'Germany':
-                for cell in itertools.islice(row, 4, None):
-                    germany_data[1].append(int(cell))
-            elif row[1] == 'Iran':
-                for cell in itertools.islice(row, 4, None):
-                    iran_data[1].append(int(cell))
-            elif row[1] == 'US':
-                # initially populate cell
-                if len(us_data[1]) == 0:
-                    for cell in itertools.islice(row, 4, None):
-                        us_data[1].append(int(cell))
-                else:
-                    # add new row to current cases
-                    for index in range(4, len(row)-4):
-                        us_data[1][index] += int(row[index])
-
-    # parse recovery case
-    with open(recov_path) as recov_file:
-        recov_reader = csv.reader(recov_file, delimiter=',')
-
-         #confirm_case_arr = [row for idx, row in enumerate(reader) if idx in ()]
-        for row in recov_reader:
-            if row[1] == 'China':
-                # initially populate cell
-                if len(china_data[2]) == 0:
-                    for cell in itertools.islice(row, 4, None):
-                        china_data[2].append(int(cell))
-                else:
-                    # add new row to current cases
-                    for index in range(4, len(row)-4):
-                        china_data[2][index] += int(row[index])
-            elif row[1] == 'Italy':
-                for cell in itertools.islice(row, 4, None):
-                    italy_data[2].append(int(cell))
-            elif row[1] == 'Germany':
-                for cell in itertools.islice(row, 4, None):
-                    germany_data[2].append(int(cell))
-            elif row[1] == 'Iran':
-                for cell in itertools.islice(row, 4, None):
-                    iran_data[2].append(int(cell))
-            elif row[1] == 'US':
-                # initially populate cell
-                if len(us_data[2]) == 0:
-                    for cell in itertools.islice(row, 4, None):
-                        us_data[2].append(int(cell))
-                else:
-                    # add new row to current cases
-                    for index in range(4, len(row)-4):
-                        us_data[2][index] += int(row[index])
     """
-    Graph Data
+     Graph Data
     """
     # convert dates to Python datetime objects
     converted_dates = [datetime.datetime.strptime(d,"%m/%d/%y").date() for d in dates]
@@ -190,10 +130,7 @@ def parse_and_graph_data():
     plt.show()
     plt.clf()
     plt.cla()
-    plt.close()
-
-
-
+    plt.close()   
 
 
 
